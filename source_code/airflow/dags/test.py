a0716792a71f1f880f6566508cbec83c94c9d855
vuhuusy <syvh.de@gmail.com>
2025-03-25 19:47:41 +0700

    test: add sample dags for airflow

--- source_code/airflow/dags/test.py
+++ source_code/airflow/dags/test.py
@@ -1,11 +1,12 @@
- import datetime
+import datetime
 
- from airflow import DAG
- from airflow.operators.empty import EmptyOperator
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
 
- my_dag = DAG(
-     dag_id="my_dag_name",
-     start_date=datetime.datetime(2021, 1, 1),
-     schedule="@daily",
- )
- EmptyOperator(task_id="task", dag=my_dag)
+my_dag = DAG(
+    dag_id="my_dag_name",
+    start_date=datetime.datetime(2021, 1, 1),
+    schedule="@daily",
+)
+
+EmptyOperator(task_id="task", dag=my_dag)
