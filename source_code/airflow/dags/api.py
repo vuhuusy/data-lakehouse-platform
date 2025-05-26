212ca282459362ee47f39e1e25630502ad171cf3
vuhuusy <vuhuusytft1@gmail.com>
2025-05-26 18:03:56 +0700

    airflow: test api cencsus

--- source_code/airflow/dags/api.py
+++ source_code/airflow/dags/api.py
@@ -1,32 +1,25 @@
 from airflow import DAG
-from airflow.providers.http.operators.http import SimpleHttpOperator
-from airflow.providers.http.hooks.http import HttpHook
 from airflow.operators.python import PythonOperator
+from airflow.providers.http.hooks.http import HttpHook
 from datetime import datetime
-import json
 
-def fetch_census_data():
-    hook = HttpHook(http_conn_id='census_api_conn', method='GET')
-    extra = hook.get_connection('census_api_conn').extra_dejson
-    api_key = extra.get("api_key")
+def fetch_census():
+    hook = HttpHook(method='GET', http_conn_id='census_api_conn')
+    conn = hook.get_connection(hook.http_conn_id)
+    api_key = conn.extra_dejson.get('api_key')
 
-    endpoint = "/data/2023/acs/acs5"
-    params = {
-        "get": "B01003_001E,NAME",
-        "for": "zip%20code%20tabulation%20area:*",
-        "key": api_key
-    }
+    response = hook.run(
+        endpoint='/data/2023/acs/acs5',
+        data={
+            "get": "B01003_001E,NAME",
+            "for": "zip code tabulation area:*",
+            "key": api_key
+        }
+    )
+    print(response.json()[:5])
 
-    response = hook.run(endpoint, data=params)
-    data = response.json()
-    print(data[:5])  # Print first 5 rows
-
-with DAG("census_api_example",
-         start_date=datetime(2024, 1, 1),
-         schedule_interval=None,
-         catchup=False) as dag:
-
-    fetch_task = PythonOperator(
-        task_id='fetch_census',
-        python_callable=fetch_census_data
+with DAG("census_api_dag", start_date=datetime(2024, 1, 1), schedule_interval=None, catchup=False) as dag:
+    t1 = PythonOperator(
+        task_id="fetch_data",
+        python_callable=fetch_census
     )
