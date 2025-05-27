f983439b7d40e02d5a10ae480aa173a47f78827b
vuhuusy <vuhuusytft1@gmail.com>
2025-05-27 19:24:49 +0700

    1

---
+++ source_code/airflow/dags/spark_pi copy.py
@@ -0,0 +1,22 @@
+import pendulum
+import yaml
+from airflow.models.dag import DAG
+from airflow.models import Variable
+from airflow.utils.dates import days_ago
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+
+with DAG(
+    dag_id="spark-pi",
+    schedule=None,
+    start_date=days_ago(2),
+    catchup=False,
+    dagrun_timeout=pendulum.duration(minutes=10),
+    tags=["spark-pi"],
+    template_searchpath=Variable.get("template_searchpath")
+) as dag:
+    spark_job = SparkKubernetesOperator(
+        task_id="spark-job-read-minio",
+        application_file="spark-jobs/spark-read-minio.yaml",
+        namespace="spark-operator",
+        kubernetes_conn_id="kubernetes_default"
+    )
