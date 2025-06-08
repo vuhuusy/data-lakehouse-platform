df88fb3d1510d474acb0c6cd659ebfc8813f4155
vuhuusy <vuhuusytft1@gmail.com>
2025-06-08 23:13:07 +0700

    1

--- source_code/airflow/dags/dag_cal_features_daily.py
+++ source_code/airflow/dags/dag_cal_features_daily.py
@@ -30,9 +30,7 @@ with DAG(
     feast_materialize = BashOperator(
         task_id="materialize_features_to_online_store",
         bash_command=(
-            "source /opt/airflow/dags/repo/source_code/airflow/feature_store/fraud_detection/venv/bin/activate && "
-            "cd /opt/airflow/dags/repo/source_code/airflow/feature_store/fraud_detection/feature_repo && "
-            "feast materialize-incremental {{ ds }}"
+            "/opt/airflow/dags/repo/feature_store/fraud_detection/venv/bin/python -m feast materialize-incremental {{ ds }}"
         )
     )
 
