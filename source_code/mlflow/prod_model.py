6ff716ef41002ad963e85a62f48c30ca68cbbb6f
vuhuusy <syvh.de@gmail.com>
2025-06-08 23:03:27 +0700

    1

--- source_code/mlflow/prod_model.py
+++ source_code/mlflow/prod_model.py
@@ -1,7 +1,7 @@
 from mlflow.tracking import MlflowClient
 client = MlflowClient()
 client.transition_model_version_stage(
-    name="fraud_detection",
+    name="FraudDetectionXGBoost",
     version=1,
     stage="Production"  # hoặc "Staging"
 )
--- source_code/mlflow/register_model.py
+++ source_code/mlflow/register_model.py
@@ -2,6 +2,6 @@ from mlflow import register_model
 import mlflow
 
 result = mlflow.register_model(
-    model_uri=f"runs:/d65ba977ee4943de80f89c2851978926/model",
-    name="fraud_detection"
+    model_uri=f"runs:/677f3271b13f445ea17e865c40171965/model",
+    name="FraudDetectionCatBoost"
 )
