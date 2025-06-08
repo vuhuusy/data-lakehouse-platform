9d71eb5281e86c7c3a807e76aa856487db9573f3
vuhuusy <vuhuusytft1@gmail.com>
2025-06-08 19:58:18 +0700

    1

--- source_code/airflow/dags/dag_cal_features_daily.py
+++ source_code/airflow/dags/dag_cal_features_daily.py
@@ -1,5 +1,5 @@
 from airflow.models.dag import DAG
-from airflow.operators.dummy import DummyOperator, BashOperator
+from airflow.operators.dummy import DummyOperator
 from airflow.models import Variable
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from datetime import datetime
@@ -26,15 +26,15 @@ with DAG(
         kubernetes_conn_id="kubernetes_default"
     )
 
-    feast_materialize = BashOperator(
-        task_id="feast_materialize_incremental",
-        bash_command=(
-            "cd /opt/airflow/dags/repo/feature_repo && "
-            "feast materialize-incremental {{ ds }}T00:00:00"
-        ),
-    )
+    # feast_materialize = BashOperator(
+    #     task_id="feast_materialize_incremental",
+    #     bash_command=(
+    #         "cd /opt/airflow/dags/repo/feature_repo && "
+    #         "feast materialize-incremental {{ ds }}T00:00:00"
+    #     ),
+    # )
 
     start = DummyOperator(task_id="start")
     end = DummyOperator(task_id="end")
 
-    start >> spark_job >> feast_materialize >> end
+    start >> spark_job >> end
