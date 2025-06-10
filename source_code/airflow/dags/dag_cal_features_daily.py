95156b8b4338d66644238a5df3776b08799712af
vuhuusy <vuhuusytft1@gmail.com>
2025-06-11 02:51:54 +0700

    1

--- source_code/airflow/dags/dag_cal_features_daily.py
+++ source_code/airflow/dags/dag_cal_features_daily.py
@@ -41,9 +41,7 @@ with DAG(
                         "AWS_ENDPOINT_URL": "https://minio.minio.svc.cluster.local:443",
                         "S3_ENDPOINT_URL": "https://minio.minio.svc.cluster.local:443",
                         "AWS_REGION": "us-east-1",
-                        "AWS_SSL_VERIFY": "false",
-                        "S3_VERIFY_SSL": "false",
-                        "PYTHONHTTPSVERIFY": "0"},
+                        "AWS_CA_BUNDLE": "/app/minio.minio.svc.cluster.download.crt"},
         is_delete_operator_pod=True,
         get_logs=True,
         kubernetes_conn_id="kubernetes_default"
