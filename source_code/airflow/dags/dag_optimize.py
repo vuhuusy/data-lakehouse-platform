9cdf98836e0c26b433a1e2151747b5b536ee64ed
vuhuusy <vuhuusytft1@gmail.com>
2025-06-15 18:37:34 +0700

    1

---
+++ source_code/airflow/dags/dag_optimize.py
@@ -0,0 +1,35 @@
+from airflow.models.dag import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.models import Variable
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from datetime import datetime
+import pendulum
+import sys
+
+sys.path.append("/opt/airflow/dags/repo/source_code/airflow/dags")
+
+default_args = {
+    'start_date': datetime(2025, 6, 1),
+    'retries': 10
+}
+
+with DAG(
+    dag_id="dag_optimize_partition",
+    schedule_interval='5 * * * *',
+    start_date=pendulum.datetime(2025, 6, 11, tz="Asia/Ho_Chi_Minh"),
+    catchup=True,
+    default_args=default_args,
+    tags=["optimize", "partition"],
+) as dag:
+    spark_job = SparkKubernetesOperator(
+        task_id="optimize_partition",
+        application_file="spark-jobs/optimize.yaml",
+        namespace="spark-operator",
+        kubernetes_conn_id="kubernetes_default"
+    )
+
+    start = DummyOperator(task_id="start")
+    end = DummyOperator(task_id="end")
+
+    start >> spark_job >> end
---
+++ source_code/airflow/dags/spark-jobs/optimize.py
@@ -0,0 +1,30 @@
+from pyspark.sql import SparkSession
+from delta.tables import DeltaTable
+
+# === Init Spark ===
+spark = SparkSession.builder \
+    .appName("Calculate Customer and Merchant Features") \
+    .config("spark.driver.extraClassPath", "/opt/bitnami/spark/jars/*") \
+    .config("spark.executor.extraClassPath", "/opt/bitnami/spark/jars/*") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.session.timeZone", "Asia/Ho_Chi_Minh") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .config("spark.hadoop.fs.s3a.endpoint", "https://minio.minio.svc.cluster.local:443") \
+    .config("spark.hadoop.fs.s3a.access.key", "minio") \
+    .config("spark.hadoop.fs.s3a.secret.key", "minio123") \
+    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("hive.metastore.uris", "thrift://hive-metastore.hive.svc.cluster.local:9083") \
+    .enableHiveSupport() \
+    .getOrCreate()
+
+# === Get partition N-1 (Vietnam time) ===
+partition = spark.sql("""
+    SELECT DATE_FORMAT(DATE_SUB(CURRENT_DATE(), 1), 'yyyyMMdd') AS partition
+""").collect()[0]['partition']
+print(f">>> 🕒 Optimizing partition = {partition}")
+
+spark.sql(f"""
+OPTIMIZE delta.`s3a://gold-zone/f_transaction_predictions`
+WHERE partition = '{partition}'
+""")
